9b01ebbbe3b21251d4b380b57c78edfa892bfe72
efeoguslu <efeoguslu@gmail.com>
2022-03-29 02:07:29 +0300

    added a new student

--- classImplementation.py
+++ classImplementation.py
@@ -8,9 +8,11 @@ class Students:
         self.grades = []
 
         for i in range(0, numGrades, 1):
-            grade = float(input('please enter grade: '))
+            prompt = "please enter grade for "+self.first+": "
+            grade = float(input(prompt))
             self.grades.append(grade)
 
+        print("end of grades for " + self.first)
         # return self.grades
 
     def printGrades(self):
@@ -18,7 +20,6 @@ class Students:
 
         for i in range(0, self.numGrades, 1):
             print(self.grades[i])
-            print(' ')
 
     def averageGrades(self):
         total = 0
@@ -26,7 +27,7 @@ class Students:
             total += self.grades[i]
         average = total/self.numGrades
         #self.average = average
-        return average
+        return round(average, 3)
 
     def lowGrade(self):
         low = 100
@@ -46,23 +47,34 @@ class Students:
 
 
 student1 = Students('efe', 'oguslu')
-
+student2 = Students('yammur', 'marla')
 
 print(student1.first, student1.last)
 
 
-numGrades = int(input("how many grades do you want: "))
+numGrades1 = int(input("how many grades do you want for efe: "))
+numGrades2 = int(input("how many grades do you want for yammur: "))
 
-student1.inputGrades(numGrades)
+student1.inputGrades(numGrades1)
+student2.inputGrades(numGrades2)
 student1.printGrades()
+student2.printGrades()
 
-average = student1.averageGrades()
-low = student1.lowGrade()
-high = student1.highGrade()
+average1 = student1.averageGrades()
+low1 = student1.lowGrade()
+high1 = student1.highGrade()
 
-print("average of grades: ", average)
-print("low grade: ", low)
-print("high grade: ", high)
+average2 = student2.averageGrades()
+low2 = student2.lowGrade()
+high2 = student2.highGrade()
+
+print("average of grades of efe: ", average1)
+print("lowest grade for efe: ", low1)
+print("highest grade for efe: ", high1)
+print(" ")
+print("average of grades of yammur: ", average2)
+print("lowest grade for yammur: ", low2)
+print("highest grade for yammur: ", high2)
 
 
 # print(testGrade)
